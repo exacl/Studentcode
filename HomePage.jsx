3b4c435ff782814d281e0b7908f7e0a10bff16f1
exacl <141098932+exacl@users.noreply.github.com>
2024-04-12 09:39:33 +0530

    Add files via upload

---
+++ HomePage.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { Link} from 'react-router-dom'
+
+export default function HomePage() {
+
+    return(
+
+        <div>
+           <Link to="/login">LOGIN</Link>
+          <br></br>
+          <Link to="/registration">REGISTER</Link>
+        </div>
+    )
+    
+}
